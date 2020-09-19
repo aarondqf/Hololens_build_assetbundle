19541e4af0ae30e0a117fdeb68548fdd57c3712e
aarond <597151520@qq.com>
2020-09-19 23:18:44 +0800

    first commit

---
+++ Assets/Editor/MyAssetBundles.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class MyAssetBundles : MonoBehaviour
+{
+    [MenuItem("Assets/BuildBundles")]//在Assets选项中添加点击按钮
+    static void BuildBundles()
+    {
+        string assetBundlesLibrary = "Assets/Bundles";
+        if (!Directory.Exists(assetBundlesLibrary))//判断本地目录是否已经存在目录名
+        {
+            Directory.CreateDirectory(assetBundlesLibrary);
+        }
+
+        BuildPipeline.BuildAssetBundles(assetBundlesLibrary, BuildAssetBundleOptions.None,
+            BuildTarget.StandaloneWindows64);//在此目录下创建AssetBundle
+    }
+}
